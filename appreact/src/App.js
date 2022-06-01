749db0b0cb5b86f7f54cffe4e210bb1065442fa2
gustavo <gustavo.felipebc@gmail.com>
2022-06-01 19:13:25 -0300

    JSX 1

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,5 +1,36 @@
+const titulo = <h1>Este é um título</h1>;
+
 const App = () => {
-  return <div>Meu app</div>;
+  const random = Math.random();
+  const ativo = false;
+
+  function mostrarNome(sobrenome) {
+    return 'Gustavo ' + sobrenome;
+  }
+
+  const carro = {
+    marca: 'Ford',
+    rodas: 4,
+  };
+
+  const estiloP = {
+    color: 'blue',
+    fontSize: '2rem',
+  };
+
+  return (
+    <>
+      {titulo}
+      <p>
+        {true ? 'adad' : 'adassadas'} - {10}
+        {mostrarNome('Felipe')}
+      </p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{(random * 1000) / 50}</p>
+    </>
+  );
 };
 
 export default App;
