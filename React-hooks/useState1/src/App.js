698b355dbba30a5a8de8880951fbd54db0d93f8f
gustavo <gustavo.felipebc@gmail.com>
2022-06-04 17:20:46 -0300

    useState1

---
+++ React-hooks/useState1/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import ButtonModal from './ButtonModal';
+import Modal from './Modal';
+
+const App = () => {
+  const [modal, setModal] = useState(false);
+
+  return (
+    <div>
+      <Modal modal={modal} setModal={setModal} />
+      <ButtonModal setModal={setModal} />
+    </div>
+  );
+};
+
+export default App;
