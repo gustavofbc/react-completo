632679cf69d1112eadbeb360e823336cc40d7504
Gustavo Felipe <gustavo.felipebc@gmail.com>
2023-09-15 14:46:32 -0300

    useEffect1

---
+++ React-hooks/useEffect1/src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import Produto from './Produto'
+
+const App = () => {
+  const [ativo, setAtivo] = useState(false)
+
+  
+
+  return (
+    <div>
+      {ativo && <Produto />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+    </div>
+  )
+}
+
+export default App
